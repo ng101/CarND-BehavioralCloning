2b3ffbacc16ec82f488e5aa7ed1e044a95ddc8df
Nitin Gupta <nitin.iitd16@gmail.com>
2017-02-14 06:14:17 +0000

    temp commit

--- model.py
+++ model.py
@@ -9,6 +9,8 @@ from keras.models import Sequential
 import pandas as pd
 from sklearn.utils import shuffle
 from sklearn.cross_validation import train_test_split
+from PIL import Image
+import math
 
 import tensorflow as tf
 tf.python.control_flow_ops = tf
@@ -16,29 +18,35 @@ tf.python.control_flow_ops = tf
 def get_model():
     model = Sequential()
     model.add(Lambda(lambda x : x/255.0 - 0.5, input_shape=(160, 320, 3), output_shape=(160, 320, 3)))
-    #model.add(Cropping2D(cropping=((50,20), (0,0))))
-    model.add(Convolution2D(24, 5, 5, subsample=(1,1), border_mode='valid'))
+    model.add(Cropping2D(cropping=((50, 20), (0,0))))
+    # Conv
+    model.add(Convolution2D(24, 5, 5, subsample=(2,2), border_mode='valid'))
     model.add(Activation('relu'))
-    model.add(MaxPooling2D((2,2)))
-    model.add(Convolution2D(36, 5, 5, subsample=(1,1), border_mode='valid'))
+    # Conv
+    model.add(Convolution2D(36, 5, 5, subsample=(2,2), border_mode='valid'))
     model.add(Activation('relu'))
-    model.add(MaxPooling2D((2,2)))
-    model.add(Convolution2D(48, 5, 5, subsample=(1,1), border_mode='valid'))
+    # Conv
+    model.add(Convolution2D(48, 5, 5, subsample=(2,2), border_mode='valid'))
     model.add(Activation('relu'))
-    model.add(MaxPooling2D((2,2)))
+    # Conv
     model.add(Convolution2D(64, 3, 3, border_mode='valid'))
     model.add(Activation('relu'))
-    model.add(Convolution2D(64, 3, 3, border_mode='valid'))
+    # Conv
+    model.add(Convolution2D(128, 3, 3, border_mode='valid'))
     model.add(Activation('relu'))
+    
+    # Flatten
     model.add(Flatten())
-    model.add(Dense(1024))
-    model.add(Activation('relu'))
-    #model.add(Dropout(0.5))
-    model.add(Dense(1024))
-    model.add(Activation('relu'))
-    #model.add(Dropout(0.5))
+    model.add(Dropout(0.5))
+    # Dense
+    model.add(Dense(128, activation='relu'))
+    # Dense
+    model.add(Dense(64, activation='relu'))
+    # Dense
+    model.add(Dense(16, activation='relu'))
     model.add(Dense(1))
     model.compile(loss='mse', optimizer='Adam', metrics=['mae'])
+    model.summary()
     return model
 
 def gen(df, batch_size, root=''):
@@ -52,13 +60,17 @@ def gen(df, batch_size, root=''):
         X = np.ndarray(shape=(len(batch), 160, 320, 3), dtype=np.float32)
         Y = np.zeros((len(batch)))
         for i in range(len(batch)):
+            flip = batch[i, 2]
             image_file = batch[i, 0].strip()
-            X[i, :, :, :] = (mpimg.imread(os.path.join(root, image_file)).astype(float))
-            Y[i] = batch[i, 1]
+            image = Image.open(os.path.join(root, image_file))
+            image_array = np.asarray(image)
+            X[i, :, :, :] = image_array if 0 == flip else np.fliplr(image_array)
+            #(mpimg.imread(os.path.join(root, image_file)).astype(float))
+            Y[i] = batch[i, 1] if 0 == flip else -1.0 * batch[i, 1]
         yield (X, Y)
 
 def train():
-    df = pd.read_csv('../training_data_2/driving_log.csv')
+    df = pd.read_csv('../tr/driving_log.csv')
     dfc = df[['center', 'steering']].rename(columns={'center':'image'})
     dfl = df[['left', 'steering']].rename(columns={'left':'image'})
     dfl['steering'] = dfl['steering'] + 0.2
@@ -66,16 +78,24 @@ def train():
     dfr['steering'] = dfr['steering'] - 0.2
     dfn = pd.concat([dfc, dfl, dfr])
     df = dfn.dropna() # beta simulator doesn't give left/right images
+    df['flip'] = 0
+    df_flipped = df.copy()
+    df_flipped['flip'] = 1
 
+    df = pd.concat([df, df_flipped])
+
+    print(df.head(1))
+    print(df['steering'].quantile([0.01,0.05,.1,.2,.3,.4,.5,.6,.7,.8,.9,.95,0.99]))
+    print(df['steering'].describe())
     df = shuffle(df)
-    train, valid = train_test_split(df, test_size = 0.33)
+    train, valid = train_test_split(df, test_size = 0.20)
     model = get_model()
-    batch_size = 128
-    root = '../training_data_2/'
+    batch_size = 256
+    root = '../tr/'
     model.fit_generator(
             gen(train, batch_size, root),
             samples_per_epoch = (len(train) // batch_size) * batch_size,
-            nb_epoch=20,
+            nb_epoch=30,
             validation_data=gen(valid, batch_size, root),
             nb_val_samples=len(valid)
             )
